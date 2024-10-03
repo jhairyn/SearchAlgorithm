a6a4d8f888a36b00517d5bf19b98e564819bb3f0
jhairyn <jhairynsobrevega22@gmail.com>
2024-10-04 01:27:42 +0800

    Add files via upload

---
+++ AliacSearchAlgo/Fringe.cs
@@ -0,0 +1,37 @@
+﻿// JHAIRYN D. SOBREVEGA
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AliacSearchAlgo;
+
+namespace AISearchSample
+{
+    class Fringe : Fringes
+    {
+        Stack<Node> s; // Use a stack for DFS
+
+        public Fringe()
+        {
+            s = new Stack<Node>(); // Initialize the stack
+        }
+
+        // Add node to the stack (push)
+        public void add(Node n, Node origin)
+        {
+            n.Origin = origin;
+            s.Push(n); // Push node onto the stack for DFS
+        }
+
+        // Remove node from the stack (pop)
+        public Node remove()
+        {
+            if (s.Count != 0)
+                return s.Pop(); // Pop the node (LIFO)
+            return null;
+        }
+    }
+}
+
---
+++ AliacSearchAlgo/Fringe2.cs
@@ -0,0 +1,37 @@
+﻿// JHAIRYN D. SOBREVEGA
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AliacSearchAlgo;
+
+namespace AISearchSample
+{
+    class Fringe2 : Fringes
+    {
+        // Use a queue for BFS
+        Queue<Node> queue;
+
+        public Fringe2()
+        {
+            queue = new Queue<Node>(); // Initialize queue
+        }
+
+        // Add node to the queue (enqueue)
+        public void add(Node n, Node origin)
+        {
+            n.Origin = origin;
+            queue.Enqueue(n); // Enqueue node for BFS
+        }
+
+        // Remove node from the front of the queue (dequeue)
+        public Node remove()
+        {
+            if (queue.Count != 0)
+                return queue.Dequeue(); // Dequeue the node in FIFO order
+            return null;
+        }
+    }
+}
+
